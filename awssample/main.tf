67b8b0e16eafab89093331920a2080c1b8da0083
production-tango <48600769+production-tango@users.noreply.github.com>
2021-09-24 19:34:36 -0700

    Change image ID

--- awssample/main.tf
+++ awssample/main.tf
@@ -14,7 +14,7 @@ variable "myTag" {
 }
 
 resource "aws_instance" "machine1" {
-    ami           = "ami-0a63cd87767e10ed4"
+    ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
     availability_zone = "us-west-1a"
     tags = {
