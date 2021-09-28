5da25638bf36c55c056145dc21aac5fb86be1f6b
production-tango <48600769+production-tango@users.noreply.github.com>
2021-09-28 13:56:33 -0700

    Update resource group name

--- azuresample/main.tf
+++ azuresample/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup1" {
-    name     = "myResourceGroup1"
+    name     = "myResourceGroup2"
     location = var.location
 
     tags = {
