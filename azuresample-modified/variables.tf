d63d83844bc6604088ec54fcf4e366fa91d85df1
production-tango <48600769+production-tango@users.noreply.github.com>
2021-10-12 19:08:43 -0600

    Create variables.tf

---
+++ azuresample-modified/variables.tf
@@ -0,0 +1,14 @@
+variable "vm_name" {
+  description = "VM Name"
+  default     = "myVM"
+}
+
+variable "myTag" {
+  description = "My Default Tag"
+  default = "terraform-test"
+}
+
+variable "location" {
+  description = "My Default Location"
+  default = "East US"
+}
