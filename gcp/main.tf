6f0608d36dcda6f77f6aee41bd57b03fc200cb90
production-tango <48600769+production-tango@users.noreply.github.com>
2021-10-22 13:12:23 -0600

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -23,6 +23,6 @@ resource "google_compute_instance" "vm_instance" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network1"
+  name                    = "terraform-network2"
   auto_create_subnetworks = "true"
 }
